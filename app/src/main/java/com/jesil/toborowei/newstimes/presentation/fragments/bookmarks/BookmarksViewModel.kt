3076593ae47a485de296c7959bab487d335809fd
Jesil OT <toboroweijesil@gmail.com>
2021-09-19 15:58:48 -1100

    Done with app navigation

---
+++ app/src/main/java/com/jesil/toborowei/newstimes/presentation/fragments/bookmarks/BookmarksViewModel.kt
@@ -0,0 +1,7 @@
+package com.jesil.toborowei.newstimes.presentation.fragments.bookmarks
+
+import androidx.lifecycle.ViewModel
+
+class BookmarksViewModel: ViewModel() {
+    //   TODO("Do something inside view model")
+}
---
+++ app/src/main/java/com/jesil/toborowei/newstimes/presentation/fragments/categories/CategoriesViewModel.kt
@@ -0,0 +1,7 @@
+package com.jesil.toborowei.newstimes.presentation.fragments.categories
+
+import androidx.lifecycle.ViewModel
+
+class CategoriesViewModel: ViewModel() {
+    //   TODO("Do something inside view Model")
+}
