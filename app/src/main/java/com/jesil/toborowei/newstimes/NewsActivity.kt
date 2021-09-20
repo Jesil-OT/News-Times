fdb0fd922dec2ff7707ab27d41f9829af10953cd
Jesil OT <toboroweijesil@gmail.com>
2021-09-19 13:27:04 -1100

    Init master branch

---
+++ app/src/main/java/com/jesil/toborowei/newstimes/NewsActivity.kt
@@ -0,0 +1,13 @@
+package com.jesil.toborowei.newstimes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+    }
+}
