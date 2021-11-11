efd9ec474ae0786518f7214310de374dccd28c20
Jesil OT <toboroweijesil@gmail.com>
2021-11-11 12:42:32 -1100

    done with general news request, and fixed layouts and bottom sheet error.

--- app/src/main/java/com/jesil/toborowei/newstimes/presentation/fragments/categories/general/GeneralFragment.kt
+++ app/src/main/java/com/jesil/toborowei/newstimes/presentation/fragments/categories/general/GeneralFragment.kt
@@ -6,27 +6,71 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.paging.CombinedLoadStates
+import androidx.paging.LoadState
 import com.jesil.toborowei.newstimes.R
+import com.jesil.toborowei.newstimes.data.models.NewsArticles
+import com.jesil.toborowei.newstimes.databinding.GeneralFragmentBinding
+import com.jesil.toborowei.newstimes.presentation.fragments.categories.CategoriesViewModel
+import com.jesil.toborowei.newstimes.presentation.utils.OpenNewsContent
+import com.jesil.toborowei.newstimes.presentation.utils.adapter.categories_adapter.CategoriesPagingAdapter
+import com.jesil.toborowei.newstimes.presentation.utils.adapter.headlines_adapter.NewsErrorHeaderFooterAdapter
+import com.jesil.toborowei.newstimes.presentation.utils.content.NewsContent
+import dagger.hilt.android.AndroidEntryPoint
 
-class GeneralFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = GeneralFragment()
+@AndroidEntryPoint
+class GeneralFragment : Fragment(R.layout.general_fragment), OpenNewsContent{
+    private val viewModel by viewModels<CategoriesViewModel>()
+    private var _binding: GeneralFragmentBinding? = null
+    private val binding get() = _binding!!
+    private val generalAdapter by lazy {
+        CategoriesPagingAdapter(requireContext(), this)
     }
 
-    private lateinit var viewModel: GeneralViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = GeneralFragmentBinding.bind(view)
+        binding.apply {
+            generalRecyclerView.apply {
+                setHasFixedSize(true)
+                adapter = generalAdapter.withLoadStateHeaderAndFooter(
+                    header = NewsErrorHeaderFooterAdapter {
+                        generalAdapter.retry()
+                    },
+                    footer = NewsErrorHeaderFooterAdapter {
+                        generalAdapter.retry()
+                    }
+                )
+            }
+            generalRetry.setOnClickListener {
+                generalAdapter.retry()
+            }
+        }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.general_fragment, container, false)
+        generalAdapter.addLoadStateListener {
+            combinedLoadStates(it)
+        }
+
+        viewModel.generalNews.observe(viewLifecycleOwner) {
+            generalAdapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(GeneralViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun combinedLoadStates(combinedLoadStates: CombinedLoadStates) = with(binding){
+        generalRecyclerView.isVisible = combinedLoadStates.source.refresh is LoadState.NotLoading
+        generalProgressBar.isVisible = combinedLoadStates.source.refresh is LoadState.Loading
+        errorGroup.isVisible = combinedLoadStates.source.refresh is LoadState.Error
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun newsContent(newsArticles: NewsArticles) {
+        val modalBottomSheet = NewsContent(newsArticles)
+        modalBottomSheet.show(parentFragmentManager, NewsContent.TAG)
+    }
 }
