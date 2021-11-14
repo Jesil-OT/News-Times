ba2fc9c2bd5edd4e4ee0a0bf6c81d45a649037d7
Jesil OT <toboroweijesil@gmail.com>
2021-11-13 13:34:22 -1100

    changed the error image to a lottie file, removed unnecessary import statement and done with categories

--- app/src/main/java/com/jesil/toborowei/newstimes/presentation/fragments/categories/health/HealthFragment.kt
+++ app/src/main/java/com/jesil/toborowei/newstimes/presentation/fragments/categories/health/HealthFragment.kt
@@ -1,11 +1,8 @@
 package com.jesil.toborowei.newstimes.presentation.fragments.categories.health
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.paging.CombinedLoadStates
